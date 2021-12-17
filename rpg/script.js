24c592fc897a09e1fdeb22e08535e564719d6fb8
Isaac Thomas <iwt1998@gmail.com>
2021-12-16 21:12:43 -0700

    Deleted rpg project

--- rpg/script.js
+++
@@ -1,6 +0,0 @@
-const text = document.getElementById("mainText")
-
-function addText(){
-    text.innerHTML += ">an angry goblin appears!<br>>[f] to flee<br>>[a] to attack<br>"
-    console.log("added")
-}

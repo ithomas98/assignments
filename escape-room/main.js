3176ffcace7560453600418c95a6e27990fd696c
Isaac Thomas <iwt1998@gmail.com>
2021-12-16 21:10:24 -0700

    Added Daily Planet Editor, Escape Room, and Event Listener Practice

---
+++ escape-room/main.js
@@ -0,0 +1,37 @@
+const readlineSync = require("readline-sync");
+let hasKey = false;
+let escaped = false;
+let count = 1;
+
+console.log("____________________________________________________________________________________________")
+console.log("You're trapped in a a room with a locked door. You see a hole in the wall and not much else.")
+
+readlineSync.setDefaultOptions({ limit: ["try the door", "reach into hole", "look for a key"] });
+
+for (i = 0; i < count; i++) {
+    let choice = readlineSync.question("what do you do? [try the door] [reach into hole] [look for a key] \n")
+
+    if (choice == "try the door") {
+        if (hasKey == true) {
+            console.log('\x1b[32m%s\x1b[0m', "You escaped!")
+            console.log("____________________________________________________________________________________________")
+            process.exit(1)
+        } else if (hasKey == false) {
+            console.log("You don't have the key")
+        }
+    }
+    else if (choice == "reach into hole") {
+        console.log('\x1b[31m%s\x1b[0m', "You touch a poison needle and die")
+        console.log("____________________________________________________________________________________________")
+        process.exit(1)
+    }
+    else if (choice == "look for a key") {
+        if (hasKey) {
+            console.log("You already found the key")
+        } else {
+            console.log("You look around for a bit and find the" + "\x1b[33m%s\x1b[0m", " key!")
+            hasKey = true;
+        }
+    }
+    count++
+}
---
+++ event-listener-practice/script.js
@@ -0,0 +1,49 @@
+const square = document.getElementById("square")
+
+square.addEventListener("mouseenter", toBlue)
+square.addEventListener("mousedown", toRed)
+square.addEventListener("mouseup", toYellow)
+square.addEventListener("dblclick", toGreen)
+document.addEventListener("wheel", toOrange)
+
+document.addEventListener("keydown", e=>{
+    console.log(e.key)
+    if(e.key == "b"){
+        toBlue()
+    }else if(e.key == "r"){
+        toRed()
+    }else if(e.key == "y"){
+        toYellow()
+    }else if(e.key == "g"){
+        toGreen()
+    }else if(e.key == "o"){
+        toOrange()
+    }
+})
+
+//Hover [x]
+function toBlue(){
+    square.style.backgroundColor = "blue"
+}
+
+//Hold Down [x]
+function toRed(){
+    square.style.backgroundColor = "red"
+}
+
+//Mouse Up [x]
+function toYellow(){
+    square.style.backgroundColor = "yellow"
+}
+
+//Double Click [x]
+function toGreen(){
+    square.style.backgroundColor = "green"
+}
+
+//Scroll [x]
+function toOrange(){
+    square.style.backgroundColor = "orange"
+}
+
+

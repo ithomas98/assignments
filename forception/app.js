43ece34daff6515f32701770b2a6cf4260fb8421
Isaac Thomas <iwt1998@gmail.com>
2021-12-28 00:03:12 -0700

    Completed Forception assignment

---
+++ forception/app.js
@@ -0,0 +1,21 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var alphabetArr = alphabet.split("")
+var outputArr = []
+
+forception(people, alphabet)
+
+function forception(people, alphabet){
+    for(i = 0; i < people.length; i++){
+        outputArr.push(people[i])
+        for(var j = 0; j < alphabetArr.length; j++){
+            outputArr.push(alphabetArr[j])
+        }
+    }
+    console.log(outputArr)
+}
+
+
+// Output:
+["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]

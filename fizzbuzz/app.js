3942aab6b75d30e9f76f3e5139c2c685c8d2a61e
Isaac Thomas <iwt1998@gmail.com>
2021-12-30 08:28:47 -0700

    Completed Fizzbuzz assignment

---
+++ fizzbuzz/app.js
@@ -0,0 +1,28 @@
+const tally = {
+    fizzbuzz: 0,
+    fizz: 0,
+    buzz: 0
+}
+
+for (let i = 1; i < 101; i++) {
+    let answer = ""
+    if (i % 15 == 0) {
+        answer = "fizzbuzz"
+        tally.fizzbuzz++
+    }
+    else if (i % 3 == 0) {
+        answer = "fizz"
+        tally.fizz++
+    }
+    else if (i % 5 == 0) {
+        answer = "buzz"
+        tally.buzz++
+    }
+    if (answer == "") {
+        answer = i
+    }
+
+    console.log(answer)
+}
+
+console.log(tally)

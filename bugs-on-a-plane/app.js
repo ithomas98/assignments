6e1957be518e2f3be6248a83008f49c27e1c510f
Isaac Thomas <iwt1998@gmail.com>
2021-12-29 21:41:25 -0700

    Completed Bugs on a Plane assignment

---
+++ bugs-on-a-plane/app.js
@@ -0,0 +1,25 @@
+var form = document["airline-form"]
+var query = document.querySelector;
+
+function formAlert() {
+    var firstName = form["first-name"].value;
+    var lastName = form["last-name"].value;
+    var age = form["age"].value;
+    var gender = form["gender"].value;
+    var location = form["travel-location"].value;
+    var diet = [];
+    if (form.elements['vegan'].checked) {
+        diet.push(document.getElementById("vegan").value);
+    }
+    if (form.elements['gluten'].checked) {
+        diet.push(document.getElementById('gluten').value);
+    }
+    if (form.elements['paleo'].checked) {
+        diet.push(document.getElementById('paleo').value);
+    }
+
+    console.log("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
+}
+
+
+form.addEventListener("click", formAlert);
